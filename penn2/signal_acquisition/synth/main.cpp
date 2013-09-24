004acdc5af3a63b0ee6312c1c8aa72df7b45f013
fireant <m.ebrahimi@ieee.org>
2013-09-24 15:01:49 -0700

    rollback synth

--- penn2/signal_acquisition/synth/main.cpp
+++ penn2/signal_acquisition/synth/main.cpp
@@ -6,7 +6,7 @@
 
 #include <zmq.hpp>
 
-#include "PO8e.h"
+//#include "PO8e.h"
 #include "GetPot.h"
 
 using namespace std;
@@ -42,18 +42,20 @@ int main(int argc, char** argv) {
 
     const size_t numberOfChannels = ifile("numberOfChannels", 0);
     cout<<"numberOfChannels: "<<numberOfChannels<<endl;
+    const size_t samplingRate = ifile("sampleRate", 6000.0);
+    cout<<"sampleRate: "<<samplingRate<<endl;
 
     // data file
     time_t rawtime;
     time(&rawtime);
     string dataFilename = string("data_")+ctime(&rawtime);
     FILE* pFile;
-    pFile = fopen("data", "wb");
+    pFile = fopen(dataFilename.c_str(), "wb");
     setvbuf (pFile, NULL, _IOFBF, numberOfChannels*sizeof(float));
 
 
     // check the PO8e card
-    PO8e *card = NULL;
+    /*PO8e *card = NULL;
     int totalNumCards = PO8e::cardCount();
     cout<<"Found "<<totalNumCards<<" card(s) in the system"<<endl;
     if (0 == totalNumCards) {
@@ -70,6 +72,7 @@ int main(int argc, char** argv) {
     card->flushBufferedData(card->samplesReady());
     // just to make sure, -1: all buffered data
     card->flushBufferedData(-1);
+    */
 
     // number of samples ready in buffer at each iteration
     size_t numberOfSamples = 0;
@@ -78,29 +81,58 @@ int main(int argc, char** argv) {
     // assuming at maximum 20 samples we are behind
     float tempBuff[numberOfChannels];
 
-    // main loop
-    while(!kbhit()) {
+    // synth signal generator
+    double (*funcp)(double) = cos;
+    // force signal frequency
+    float forceFrq = 0.25;
 
-        // how many samples are buffered
-        numberOfSamples = card->samplesReady();
-        cout<<"#buffered samples: "<<numberOfSamples<<endl;
+    size_t secondsOfData = 10;
+    for (size_t t=0; t<(10*samplingRate); t++) {
+        // main loop
+        //while(!kbhit()) {
 
-        for(size_t i = 0; i < numberOfSamples; i++) {
-            if (card->readBlock(tempBuff, 1) != 1) {
-                cout<<"reading sample "<<i<<" failed!"<<endl;
-                return 1;
+            // how many samples are buffered
+            //numberOfSamples = card->samplesReady();
+            numberOfSamples = 1;
+            cout<<"#buffered samples: "<<numberOfSamples<<endl;
+
+            for(size_t i = 0; i < numberOfSamples; i++) {
+                /*if (card->readBlock(tempBuff, 1) != 1) {
+                     cout<<"reading sample "<<i<<" failed!"<<endl;
+                     return 1;
+                }
+                // advance PO8e buffer pointer
+                card->flushBufferedData(1);
+                */
+                // simulate:
+                // ch 1: force sensor
+                tempBuff[0] = fabs((*funcp)(2.0*M_PI * float(t)/float(samplingRate)
+                                       * forceFrq));
+                // ch 2: digital in
+                tempBuff[1] = 0.0;
+                // ch 3: zero
+                tempBuff[2] = 0.0;
+                // ch 4: zero
+                tempBuff[3] = 0.0;
+
+                // correlated channels
+                for (size_t ch=4; ch<8; ch++) {
+                    tempBuff[ch] = (*funcp)(2.0*M_PI * float(t)/float(samplingRate) * float(ch)*4.0 ) * tempBuff[0];
+                }
+                // uncorrelated channels
+                for (size_t ch=8; ch<20; ch++) {
+                    tempBuff[ch] = (*funcp)(2.0*M_PI * float(t)/float(samplingRate) * float(ch)*4.0 ) * 1.0;
+                }
+
+                // write into file
+                fwrite(&tempBuff, sizeof(float), numberOfChannels, pFile);
             }
-            // advance PO8e buffer pointer
-            card->flushBufferedData(1);
 
-            // write into file
-            fwrite(&tempBuff, sizeof(float), numberOfChannels, pFile);
-        }
-
-    } // main loop
+        //} // main loop
+    } // simulation loop
 
     fclose(pFile);
-    PO8e::releaseCard(card);
+    //PO8e::releaseCard(card);
 
     return 0;
 }
