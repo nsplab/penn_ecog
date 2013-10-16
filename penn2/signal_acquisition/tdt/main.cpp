cb763ed529464f2d89fdc674339d0a6c0dfce21e
rsrinivasan02 <rsrinivasan02@hotmail.com>
2013-10-16 14:52:59 -0700

    Update main.cpp
    group commenting with mosalam/leon/ram

--- penn2/signal_acquisition/tdt/main.cpp
+++ penn2/signal_acquisition/tdt/main.cpp
@@ -5,18 +5,19 @@
 #include <iostream>
 #include <stdint.h>
 
-#include <zmq.hpp>
+#include <zmq.hpp>  //provides protocol for communicating between different modules in the ECoG system
 
-#include "PO8e.h"
-#include "GetPot.h"
-#include "zhelpers.hpp"
+#include "zhelpers.hpp"  //helper functions used by the program in conjunction with zmq
+#include "PO8e.h"   //code provided by TDT to stream/read signal amplitudes
+#include "GetPot.h" //code for reading config files (eg. signal.cfg) that contain parameters for the experiment
+
 
 using namespace std;
 using namespace zmq;
 
 string cfgFile("penn.cfg");
 
-int kbhit()
+int kbhit()	//detects if keyboard is hit or not. this is used to press any key to close files and exit the code
 {
     struct timeval tv = { 0L, 0L };
     fd_set fds;
@@ -29,23 +30,26 @@ int main(int argc, char** argv) {
     // parse command line arguments
     GetPot cl(argc, argv);
 
-    // check if config exists
+    // check if the config file exists (extension .cfg)
     ifstream testCfgFile(cfgFile.c_str());
     if (! testCfgFile.good()) {
-        cout<<"Could not open the config file: "<<cfgFile<<endl;
+        cout<<"Could not open the config file needed to set recording parameters: "<<cfgFile<<endl;
         return 1;
     } else {
         testCfgFile.close();
     }
 
-    // parse config file
+    // parse config file to load parameter values
     GetPot ifile(cfgFile.c_str(), "#", "\n");
-    ifile.print();
+    ifile.print();	//print the config file to the terminal for user reference
 
-    const size_t numberOfChannels = ifile("numberOfChannels", 0);
-    cout<<"numberOfChannels: "<<numberOfChannels<<endl;
+    const size_t numberOfChannels = ifile("numberOfChannels", 0);	//load total number of channels (ECoG plus any other channels)
+    cout<<"numberOfChannels: "<<numberOfChannels<<endl;			//print number of channels to the screen
 
-    // data file
+    // define the data file that will contain all data received by the TDT,
+    //timestamped using a counter running on the PC (Puget)
+    //form the file name in the format "data_wed_16.10.2013_14:41:59"
+    //open the file - form the variables record all data received by the TDT
     time_t rawtime;
     time(&rawtime);
     char nameBuffer[24];
@@ -57,30 +61,38 @@ int main(int argc, char** argv) {
     setvbuf (pFile, NULL, _IOFBF, numberOfChannels*sizeof(float));
 
 
-    // check the PO8e card
+    // check the PO8e card to:
+    // 	find the card
+    //  check if it can connect to the hardware using the TDT-provided "kernel module"
+    //	various things can cause this step to fail:
+    //		inadvertently upgrading the Linux kernel version on the PC
+    //		PO8e card removed or damaged
+    //		TDT PO8e kernel module is removed or not loaded
     PO8e *card = NULL;
-    int totalNumCards = PO8e::cardCount();
+    int totalNumCards = PO8e::cardCount();	//check the number of PO8e cards installed in the PC (Puget)
     cout<<"Found "<<totalNumCards<<" card(s) in the system"<<endl;
     if (0 == totalNumCards) {
         cout<<"Did not find any PO8e cards"<<endl;
         return 1;
     }
-    card = PO8e::connectToCard(0);
-    if (! card->startCollecting()) {
+    card = PO8e::connectToCard(0);	//returns a pointer to an instance of the PO8e class
+    if (! card->startCollecting()) {	//connectToCard(0) checks card 0
         cout<<"startCollecting() failed with: "<<card->getLastError()<<endl;
         PO8e::releaseCard(card);
     }
 
-    // release buffer
-    card->flushBufferedData(card->samplesReady());
-    // just to make sure, -1: all buffered data
-    card->flushBufferedData(-1);
+    // clear contents of the buffer containing data received by the PO8e
+    //one way to flush the buffer
+    card->flushBufferedData(card->samplesReady());  
+    						    //remove a certain number of samples from the buffer specified by samplesRead()
+    //another way to flush the buffer, just to be sure
+    card->flushBufferedData(-1);	  //flushBufferedData(-1) flushes all data in the buffer
 
     // number of samples ready in buffer at each iteration
-    size_t numberOfSamples = 0;
+    size_t numberOfSamples = 0;	//initialized to zero, but will be updated later
 
-    // temp buffer to copy date from PO8e's buffer
-    // assuming at maximum 20 samples we are behind
+    // temporary buffer to copy data from PO8e's buffer.
+    // this buffer only contains one float per channel
     float tempBuff[numberOfChannels];
 
     context_t context(1);
