1af9fa883bda934a97d092db495a3341e607096c
fireant <m.ebrahimi@ieee.org>
2013-11-20 15:26:13 -0800

    cardgame debug

--- penn2/graphics/cardgame/main.cpp
+++ penn2/graphics/cardgame/main.cpp
@@ -105,7 +105,7 @@ void powerTh()
 //cout<<"recvd"<<endl;
 //  cout<<"sz "<<sig_msg.size()<<endl;
 //  memcpy(&timeStamp, sig_msg.data(), sizeof(size_t));
-//cout<<"timestamp: "<<timeStamp<<endl;
+cout<<"timestamp: "<<timeStamp<<endl;
 
   memcpy(buffer, (size_t*)sig_msg.data()+1, sig_msg.size()-sizeof(size_t));
   //cout<<"done"<<endl;
