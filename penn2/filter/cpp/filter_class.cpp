9fa89a05f6c8abe7c3eaff0014754a2c02cdecf3
rsrinivasan02 <rsrinivasan02@hotmail.com>
2014-04-07 13:46:25 -0700

    additional filter commenting in filter_class.cpp by Ram

--- penn2/filter/cpp/filter_class.cpp
+++ penn2/filter/cpp/filter_class.cpp
@@ -19,19 +19,20 @@ FilterClass::FilterClass() {
   handPos_.resize(3, 0.0);
 }
 
-void FilterClass::GrabFeatures() {
+void FilterClass::GrabFeatures() {                                                          // receives new features via zmq from the feature_extraction module
   // receive data from feature extractor
-  zmq::message_t features_msg;
-  features_subscriber_.recv(&features_msg);
+  zmq::message_t features_msg;                                                              // define a new object of the message_t class in the zmq namespace
+  features_subscriber_.recv(&features_msg);                                                 // wait for the next new message in features_msg (this is "blocking" request;
+                                                                                            // it won't proceed until a new value is detected by recv())
+                                                                                            // features_subscriber_ is initialized in the FilterClass constructor to connect to "ipc:///tmp/features.pipe"
 
-  // extract timestamp, # features, and features
-  memcpy(&featureTimestamp_, features_msg.data(), sizeof(size_t));
+  // extract timestamp, # features, and feature values
+  memcpy(&featureTimestamp_, features_msg.data(), sizeof(size_t));                          // copy sizeof(size_t) bytes from feature_msg.data() into featureTimestamp_, which is an unsigned integer of data type size_t
+  cout<<"timestamp: "<<featureTimestamp_<<endl;                                             // (debug code) print the timestamp to the console
 
-  cout<<"timestamp: "<<featureTimestamp_<<endl;
+  size_t vec_size = (features_msg.size() - sizeof(size_t)) / sizeof(float);                 // calculate how long (in # of floats) the feature message is, minus the size of the featureTimestamp that was already extracted
 
-  size_t vec_size = (features_msg.size() - sizeof(size_t)) / sizeof(float);
-
-  cout<<"vec size: "<<vec_size<<endl;
+  cout<<"vec size: "<<vec_size<<endl;                                                       // (debug code) print the number of floats left in the message to console
 
   if (features_.size() != vec_size) {
     features_.resize(vec_size);
@@ -45,45 +46,48 @@ void FilterClass::GrabFeatures() {
   cout<<endl;
 }
 
-void FilterClass::SendHandPosGetState(const vector<float>& hand_movement) {
-    stringstream message;
-    message<<featureTimestamp_<<" ";
-    for (vector<float>::const_iterator it=hand_movement.begin();
-         it<hand_movement.end(); it++) {
+void FilterClass::SendHandPosGetState(const vector<float>& hand_movement) {                 // purpose: send updated hand position estimate from filter, and receive resulting hand position from supervisor.
+                                                                                            // input:    hand_movement (passed by reference)
+                                                                                            // function: uses ZMQ to send command from filter to supervisor on hand placement
+                                                                                            //           receives the hand position set by the supervisor, which may be different (such as when velocity is integrated)
+    stringstream message;                                                                   // message is a stringstream that will contain featureTimestamp and hand_movement data
+    message<<featureTimestamp_<<" ";                                                        // pack the featureTimestamp into message
+    for (vector<float>::const_iterator it=hand_movement.begin();                            // iterate over the array values in hand_movement and copy them to the message stringstream
+         it<hand_movement.end(); it++) {input
       message<<*it<<" ";
     }
-    message<<endl;
-    zmq::message_t zmq_message(message.str().length());
-    memcpy((char *) zmq_message.data(), message.str().c_str(), message.str().length());
-    supervisor_.send(zmq_message);
-    cout<<"message sent"<<endl;
+    message<<endl;                                                                          // complete the message stringstream with an endl character.
+    zmq::message_t zmq_message(message.str().length());                                     // create a zmq message_t to send the message to the supervisor
+    memcpy((char *) zmq_message.data(), message.str().c_str(), message.str().length());     // insert the message stringstream into the message_t object zmq_message
+    supervisor_.send(zmq_message);                                                          // use the supervisor ipc connection (established in constructor for FilterClass) to send the message
+    cout<<"message sent from filter to supervisor"<<endl;                                   // (debug code) print to the console that the message was sent from the filter to the supervisor
 
-    // receive data from supervisor
-    zmq::message_t supervisor_msg;
-    supervisor_.recv(&supervisor_msg);
-    // convert received data into c++ string/sstream
-    string feat_str;
-    feat_str.resize(supervisor_msg.size(),'\0');
-    feat_str.assign((char *)supervisor_msg.data(),supervisor_msg.size());
-    cout<<"feat_str "<<feat_str<<endl;
+                                                                                            // receive data from supervisor that contains the updated state of the hand in the virtual environment
+    zmq::message_t supervisor_msg;                                                          // define a message_t to receive the supervisor message
+    supervisor_.recv(&supervisor_msg);                                                      // use the supervisor ipc connection (established in the constructor for FilterClass) to receive the message (blocking)
+
+    string supervisor_msg_str;                                                              // create a string to receive the supervisor message
+    supervisor_msg_str.resize(supervisor_msg.size(),'\0');                                  // define the string size based on the size of the supervisor message
+    supervisor_msg_str.assign((char *)supervisor_msg.data(),supervisor_msg.size());         // convert the received supervisor message into c++ string/sstream
+    cout<<"feat_str "<<supervisor_msg_str<<endl;                                            // (debug code) print to the console what the supervisor string was
     //replace(feat_str.begin(), feat_str.end(), ',', ' ');
-    stringstream ss(feat_str);
+    stringstream supervisor_msg_ss(supervisor_msg_str);                                     //convert the supervisor_msg_str into a stringstream for convenient parsing
 
     // extract target position, hand position, trial ID, mode (training/testing)
     // and attending value from supervisor's message
-    ss >> target_[0];ss >> target_[1];ss >> target_[2];
+    supervisor_msg_ss >> target_[0];supervisor_msg_ss >> target_[1];supervisor_msg_ss >> target_[2];             //without loss of generality, store the target position in 3-dim space (even for 2D or 1D mode)
 
-    ss >> handPos_[0];ss >> handPos_[1];ss >> handPos_[2];
+    supervisor_msg_ss >> handPos_[0];supervisor_msg_ss >> handPos_[1];supervisor_msg_ss >> handPos_[2];          //without loss of generality, store the hand position in 3-dim space (even for 2D or 1D mode)
 
-    ss >> trial_id;
+    supervisor_msg_ss >> trial_id;
 
     cout<<"trial_id "<<trial_id<<endl;
 
     int mode;
-    ss >> mode;
+    supervisor_msg_ss >> mode;
     mode_ = (TrialMode)mode;
 
-    ss >> attending_;
+    supervisor_msg_ss >> attending_;
 }
 
 void FilterClass::Simulate(vector<float> features, size_t trial, vector<float> target, std::vector<float> initHandPosition) {
--- penn2/filter/cpp/filter_class.h
+++ penn2/filter/cpp/filter_class.h
@@ -9,36 +9,30 @@ enum TrialMode {TRAINING, TESTING};
 class FilterClass {
 public:
   FilterClass();
-  // receive features from the feature extraction module
-  void GrabFeatures();
-  // receive state from supervisor:
-  // hand position, mode (training/testing), target position
-  void SendHandPosGetState(const std::vector<float>& hand_movement);
+  void GrabFeatures();                                              // receive features from the feature extraction module
+  void SendHandPosGetState(const std::vector<float>& hand_movement);// receive virtual hand state from supervisor: hand position, mode (training/testing), target position
 
-  // derived classes should implement these two methods
-  virtual void Update() = 0;
-  virtual void Predict() = 0;
-
+                                                                    // classes derived from FilterClass should implement these two methods
+  virtual void Update() = 0;                                        // update step of a recursive filter (named with Bayesian filters in mind)
+  virtual void Predict() = 0;                                       // prediction step of a recursive filter (named with Bayesian filters in mind)
+                                                                    // setting the function to 0 tells the compiler there is no implementation for these functions
+                                                                    // this prevents the compiler from creating objects of this base class
+                                                                    // we're expecting to create derived classes that implement these functions (like naive_filter)
   // auxiliary function to test the filter
   void Simulate(std::vector<float> features, size_t trial, std::vector<float> target, std::vector<float> initHandPosition);
-  std::vector<float> GetHandState() {return handState_;}
+  std::vector<float> GetHandState() {return handState_;}            // returns the handState_
 protected:
-  // feature vector
-  std::vector<float> features_;
-  // hand position
-  std::vector<float> handPos_;
-  // hand state
-  std::vector<float> handState_;
-  // training or testing mode
-  TrialMode mode_;
-  // subject attending
-  float attending_;
-  // trial ID sent by supervisor
-  // should be sent back to it
-  size_t trial_id;
+  std::vector<float> features_;                                     // feature vector at the current point in time
+  std::vector<float> handPos_;                                      // update-step-based hand position intended to be sent to the virtual environment at the current point in time
+  std::vector<float> handState_;                                    // hand state variable - could be used to record position and velocity
+
+  TrialMode mode_;                                                  // trial mode - training (adaptive version of JointRSE) or testing (static version of JointRSE) mode
+  float attending_;                                                 // whether the subject is attending to the task; future versions of the code will use head tracking, etc. to check if the
+                                                                    // subject is attending.
+  size_t trial_id;                                                  // trial ID sent by supervisor; should be sent back to it
   // target position sent by supervisor
   std::vector<float> target_;
-  size_t featureTimestamp_;
+  size_t featureTimestamp_;                                         //timestamping for each feature is based on a counter rather than system time. the size_t data type is an unsigned integer value.
   size_t dim_;
 private:
   static zmq::context_t context_;
