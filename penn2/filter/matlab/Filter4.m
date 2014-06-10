4881aba10283665ebb29055de14a73618e0166e7
fireant <m.ebrahimi@ieee.org>
2014-06-10 11:28:14 -0700

    some new changes

---
+++ penn2/filter/matlab/Filter4.m
@@ -0,0 +1,19 @@
+classdef Filter4 < FilterClass
+    properties
+    end
+        methods
+        % class constructor
+        function filter = Filter4(varargin)
+            filter.speed = 2;
+            filter.ssave = struct();
+            filter.parameterNames = {'alpha', 'beta'};
+        end
+        % function that is called every iteration when new
+        % feature values are received from the feature extractor
+        function filter=RunFilter(filter, val)
+            filter.speed = filter.speed + 1;
+             filter.parameters{1,1} = filter.speed;
+             filter.parameters{1,2} = filter.speed + val;
+        end
+    end
+end
