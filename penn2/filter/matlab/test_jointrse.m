37daa499bfadf7e0b018f7f839a164e15cf3d9fd
Bryan He <bryanhe@caltech.edu>
2014-07-07 14:04:26 -0700

    Fixed problem with deleting velocity from target

--- penn2/filter/matlab/test_jointrse.m
+++ penn2/filter/matlab/test_jointrse.m
@@ -7,7 +7,7 @@ function [] = test_jointrse()
   filter.setHandPos(hand);
   for i = 1:100
     target = normrnd(0, 1);
-    filter.target = target;
+    filter.target(1) = target;
     obs = (target - hand) / 10;
     features = sqrt(obs + imitatorBaseline) * imitatorAmplifier;
     filter.RunFilter(features);
