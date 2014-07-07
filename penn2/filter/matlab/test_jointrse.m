2b1108cc3622fb04cee559664f980c1fcff789a5
Bryan He <bryanhe@caltech.edu>
2014-07-07 14:00:17 -0700

    Test code for JointRSE

---
+++ penn2/filter/matlab/test_jointrse.m
@@ -0,0 +1,16 @@
+function [] = test_jointrse()
+  imitatorBaseline = 50; 
+  imitatorAmplifier = 2; 
+
+  filter = FilterJointRSE(1, 1);
+  hand = 0;
+  filter.setHandPos(hand);
+  for i = 1:100
+    target = normrnd(0, 1);
+    filter.target = target;
+    obs = (target - hand) / 10;
+    features = sqrt(obs + imitatorBaseline) * imitatorAmplifier;
+    filter.RunFilter(features);
+  end
+end
+
