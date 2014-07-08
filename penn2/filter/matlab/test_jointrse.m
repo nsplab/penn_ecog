8d6c83e456b13f537c517536371f69caf9d76245
Bryan He <bryanhe@caltech.edu>
2014-07-08 10:25:10 -0700

    Fixed bug where only the x hand position was being used for all control signals

--- penn2/filter/matlab/test_jointrse.m
+++ penn2/filter/matlab/test_jointrse.m
@@ -2,15 +2,16 @@ function [] = test_jointrse()
   imitatorBaseline = 50; 
   imitatorAmplifier = 2; 
 
-  filter = FilterJointRSE(1, 1);
-  hand = 0;
+  dimensions = 3;
+  channels = dimensions;
+
+  filter = FilterJointRSE(dimensions, channels);
+  hand = zeros(dimensions, 1);
   filter.setHandPos(hand);
-  dimensions = 1;
-  channels = 1;
   for i = 1:1000
-    target = normrnd(0, 1);
-    filter.target(1) = target;
-    hand = filter.parameterValues{((dimensions + 1) * channels) + (1:dimensions)}
+    target = normrnd(0, 1, dimensions, 1);
+    filter.target(1:dimensions) = target;
+    hand = [filter.parameterValues{((dimensions + 1) * channels) + (1:dimensions)}]'
     obs = (target - hand) / 10;
     features = sqrt(obs + imitatorBaseline) * imitatorAmplifier;
     filter.RunFilter(features);
