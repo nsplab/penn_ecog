1da60aea925dfc95accc0d0c1be40d774d963716
Bryan He <bryanhe@caltech.edu>
2014-07-10 13:04:41 -0700

    Add noise and simulate reaches, rather than random targets

--- penn2/filter/matlab/test_jointrse.m
+++ penn2/filter/matlab/test_jointrse.m
@@ -8,11 +8,14 @@ function [] = test_jointrse()
   filter = FilterJointRSE(dimensions, channels);
   hand = zeros(dimensions, 1);
   filter.setHandPos(hand);
-  for i = 1:1000
-    target = normrnd(0, 1, dimensions, 1);
+  for i = 1:3000
+    if (mod(i, 100) == 1)
+      target = normrnd(0, 1, dimensions, 1);
+    end
     filter.target(1:dimensions) = target;
-    hand = [filter.parameterValues{((dimensions + 1) * channels) + (1:dimensions)}]'
-    obs = (target - hand) / 10;
+    hand = [filter.parameterValues{((dimensions + 1) * channels) + (1:dimensions)}]';
+    sigma = 0.1;
+    obs = (target - hand) / 10 + normrnd(0, sigma, dimensions, 1);
     features = sqrt(obs + imitatorBaseline) * imitatorAmplifier;
     filter.RunFilter(features);
     filter.LogParameters('/home/bryanhe/penn_ecog/penn2/data', 'JointRSE');
